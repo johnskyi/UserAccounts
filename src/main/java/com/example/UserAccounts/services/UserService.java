a700408fca65fde3273168ebcd672476dee5b5c1
johnskyi <johnskyi2@yandex.ru>
2021-08-31 00:40:06 +0300

    added log4j2

--- src/main/java/com/example/UserAccounts/services/UserService.java
+++ src/main/java/com/example/UserAccounts/services/UserService.java
@@ -3,6 +3,10 @@ package com.example.UserAccounts.services;
 import com.example.UserAccounts.entities.UserEntity;
 import com.example.UserAccounts.repositories.UserRepo;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +19,11 @@ import java.util.*;
 @Service
 @AllArgsConstructor
 public class UserService {
+
+    private static final Marker ADD_HISTORY_MARKER = MarkerManager.getMarker("ADD_HISTORY");
+    private static final Marker UPDATE_USER_MARKER = MarkerManager.getMarker("UPDATE_USER");
+    private static final Marker BLOCKED_USER_MARKER = MarkerManager.getMarker("BLOCKED_USER");
+    private static final Logger LOGGER = LogManager.getLogger(UserService.class);
 
 
     @Autowired
@@ -33,6 +42,7 @@ public class UserService {
                 .passWord(userMap.get("passWord"))
                 .build();
         userRepo.save(user);
+        LOGGER.info(ADD_HISTORY_MARKER, "Добавлен пользователь: {}", user.getId());
         String message = "{\n" +
                 "  \"id\": " + user.getId() + ",\n" +
                 "  \"name\":\"" + user.getName() + "\",\n" +
@@ -40,6 +50,7 @@ public class UserService {
                 "  \"passWord\":\"" + user.getPassWord() + "\",\n" +
                 "  \"isBlocked\":" + user.isBlocked() + "\n" +
                 "}";
+
         template.convertAndSend("anyQueue", message);
 
         return ResponseEntity.ok(userRepo.findById(user.getId()).get());
@@ -60,6 +71,8 @@ public class UserService {
         userOptional.get().setPassWord(userMap.get("passWord"));
 
         userRepo.save(userOptional.get());
+
+        LOGGER.info(UPDATE_USER_MARKER, "Обновлены данные пользователя {}", userOptional.get().getId());
 
 
         String message = "{\n" +
@@ -89,6 +102,8 @@ public class UserService {
         userOptional.get().setBlocked(true);
 
         userRepo.save(userOptional.get());
+
+        LOGGER.info(BLOCKED_USER_MARKER, "Заблокирован пользователь: {}", userOptional.get().getId());
 
 
         String message = "{\n" +
